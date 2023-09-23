29db9a396991f118e1f2310b13a42ff3ea36f37e
Chanchana Sornsoontorn <off9955555@gmail.com>
2023-09-23 21:38:49 +0700

    add hero image to auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar/Navbar'
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
@@ -6,8 +7,11 @@ type Props = {}
 export default function AuthPage({}: Props) {
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
-      <div className="max-w-7xl m-auto">
+      <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <Image src="/hero.png" alt="Hero img" width={700} height={700} />
+        </div>
       </div>
     </div>
   )
