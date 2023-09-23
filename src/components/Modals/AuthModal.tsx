b69b068e1330e3c26858b1c6b2682a4aada622ef
Chanchana Sornsoontorn <off9955555@gmail.com>
2023-09-23 22:09:12 +0700

    mock login and signup page

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,6 +1,8 @@
 // import { authModalState } from "@/atoms/authModalAtom";
 import React, { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
+import Login from "./Login";
+import Signup from "./Signup";
 // import Login from "./Login";
 // import ResetPassword from "./ResetPassword";
 // import Signup from "./Signup";
@@ -30,6 +32,8 @@ const AuthModal: React.FC<AuthModalProps> = () => {
 							</button>
 						</div>
 						{/* {authModal.type === "login" ? <Login /> : authModal.type === "register" ? <Signup /> : <ResetPassword />} */}
+						{/* <Login /> */}
+						<Signup />
 					</div>
 				</div>
 			</div>
