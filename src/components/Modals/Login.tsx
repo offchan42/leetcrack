a48b3af1d63fda7cddf7b6d35145877de175a6cd
Chanchana Sornsoontorn <off9955555@gmail.com>
2023-09-24 23:42:29 +0700

    chore: impl login

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,44 +1,48 @@
-import { authModalState } from "@/atoms/authModalAtom";
-// import { auth } from "@/firebase/firebase";
-// import { useRouter } from "next/router";
+import { authModalState } from '@/atoms/authModalAtom'
+import { auth } from '@/firebase/firebase'
+import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-// import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { useSetRecoilState } from "recoil";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useSetRecoilState } from 'recoil'
 // import { toast } from "react-toastify";
 type LoginProps = {}
 
 const Login: React.FC<LoginProps> = () => {
-  const setAuthModalState = useSetRecoilState(authModalState);
-  const handleClick = (type: "login" | "register" | "forgotPassword") => {
-  	setAuthModalState((prev) => ({ ...prev, type }));
-  };
-  // const [inputs, setInputs] = useState({ email: "", password: "" });
-  // const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
-  // const router = useRouter();
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  // 	setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  // };
+  const setAuthModalState = useSetRecoilState(authModalState)
+  const handleClick = (type: 'login' | 'register' | 'forgotPassword') => {
+    setAuthModalState((prev) => ({ ...prev, type }))
+  }
+  const [inputs, setInputs] = useState({ email: '', password: '' })
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth)
+  const router = useRouter()
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
 
-  // const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-  // 	e.preventDefault();
-  // 	if (!inputs.email || !inputs.password) return alert("Please fill all fields");
-  // 	try {
-  // 		const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
-  // 		if (!newUser) return;
-  // 		router.push("/");
-  // 	} catch (error: any) {
-  // 		toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
-  // 	}
-  // };
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!inputs.email || !inputs.password)
+      return alert('Please fill all fields')
+    try {
+      const newUser = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      )
+      if (!newUser) return
+      router.push('/')
+    } catch (error: any) {
+      alert(error.message)
+      // toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
+    }
+  }
 
-  // useEffect(() => {
-  // 	if (error) toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
-  // }, [error]);
+  useEffect(() => {
+    // if (error) toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
+    if (error) alert(error.message)
+  }, [error])
   return (
-    <form
-      className="space-y-6 px-6 pb-4"
-      // onSubmit={handleLogin}
-    >
+    <form className="space-y-6 px-6 pb-4" onSubmit={handleLogin}>
       <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
       <div>
         <label
@@ -48,7 +52,7 @@ const Login: React.FC<LoginProps> = () => {
           Your Email
         </label>
         <input
-          // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="email"
           name="email"
           id="email"
@@ -67,7 +71,7 @@ const Login: React.FC<LoginProps> = () => {
           Your Password
         </label>
         <input
-          // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="password"
           name="password"
           id="password"
@@ -85,8 +89,7 @@ const Login: React.FC<LoginProps> = () => {
                 text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
             "
       >
-        {/* {loading ? 'Loading...' : 'Log In'} */}
-				Log In
+        {loading ? 'Loading...' : 'Log In'}
       </button>
       <button
         className="flex w-full justify-end"
